ed87a9995dd33a8405c345a4b93dd9564a2de2c5
Cedie James A. Padua <cediejamesapadua@gmail.com>
2026-07-07 13:32:11 +0800

    Implement changing of colors of guessing color balls

--- 04-guessing-game/script.js
+++ 04-guessing-game/script.js
@@ -1,4 +1,27 @@
-const COLORS_LIST = ["red", "green", "blue", "yellow"]
+/**
+ * OVERVIEW ---
+ * 
+ * Generate colors to be guessed.
+ * 
+ * If a guess color ball is clicked, change its color and value.
+ * 
+ * If guess button is clicked, add a set of colored balls below
+ * the guess color ball and indicate how many of the 
+ * guesses are right. Then, reset the guess color balls.
+ * 
+ * If all of the guesses are right, reveal the answer balls and
+ * show a play again button.
+ * 
+ * If surrender button is clicked, reveal the answer balls and
+ * show a play again button.
+ * 
+ * COLOR CHANGING LOGIC ---
+ * 
+ */
+
+const COLORS_LIST = ["red", "green", "blue", "yellow"];
+const COLORS_COUNT = 4;
+const guessingColorBalls = document.querySelectorAll("div.guessing-container > div");
 
 function getRandomColor(listOfColors) {
    const randomNumber = Math.random();
@@ -16,3 +39,22 @@ function getRandomColors(numberOfColors) {
 
    return randomColors;
 }
+
+function getNextColor(currentColor) {
+   const currentIndex = COLORS_LIST.findIndex((color) => color === currentColor);
+
+   if(currentIndex < 3) {
+      return COLORS_LIST[currentIndex + 1]
+   }
+   
+   return "red";
+}
+
+guessingColorBalls.forEach((guessingColorBall) => {
+   guessingColorBall.addEventListener("click", () => {
+      const nextColor = getNextColor(guessingColorBall.attributes.color.value)
+
+      guessingColorBall.style.backgroundColor = nextColor;
+      guessingColorBall.attributes.color.value = nextColor;
+   })
+})
