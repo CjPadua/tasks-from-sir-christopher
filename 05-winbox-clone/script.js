976f6ecb708315e18c1e113efb5370cf23ced289
Cedie James A. Padua <cediejamesapadua@gmail.com>
2026-07-10 12:59:35 +0800

    Stylize and transfer event listener to header title

--- 05-winbox-clone/script.js
+++ 05-winbox-clone/script.js
@@ -9,7 +9,51 @@ function openWindow (e) {
    newWindow.style.zIndex = ++globalZIndex;
    newWindow.style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`
 
-   document.body.appendChild(newWindow)
+   const header = document.createElement("div");
+   header.classList.add("window-header");
+
+   const headerTitle = document.createElement("p");
+   headerTitle.classList.add("window-header-title");
+   headerTitle.textContent = e.currentTarget.value;
+   header.appendChild(headerTitle);
+
+   const minimizeButton = document.createElement("button");
+   minimizeButton.innerHTML = `__
+   `;
+   header.appendChild(minimizeButton);
+   
+   const maximizeButton = document.createElement("button");
+   maximizeButton.innerHTML = `
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-square" viewBox="0 0 16 16">
+  <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+</svg>
+   `;
+   header.appendChild(maximizeButton);
+
+   const fullScreenButton = document.createElement("button");
+   fullScreenButton.innerHTML = `
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-fullscreen" viewBox="0 0 16 16">
+  <path d="M1.5 1a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4A1.5 1.5 0 0 1 1.5 0h4a.5.5 0 0 1 0 1zM10 .5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 16 1.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5M.5 10a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 0 14.5v-4a.5.5 0 0 1 .5-.5m15 0a.5.5 0 0 1 .5.5v4a1.5 1.5 0 0 1-1.5 1.5h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5"/>
+</svg>
+   `;
+   header.appendChild(fullScreenButton);
+
+   const closeScreenButton = document.createElement("button");
+   closeScreenButton.innerHTML = `
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+  <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+</svg>
+   `;
+   header.appendChild(closeScreenButton);
+
+   newWindow.appendChild(header);
+
+   const windowBody = document.createElement("div");
+   windowBody.classList.add("window-body");
+
+   newWindow.appendChild(windowBody);
+
+   document.body.appendChild(newWindow);
 
    let currentX = Math.random() * 100 + 50;
    let currentY = Math.random() * 100 + 50;
@@ -26,7 +70,7 @@ function openWindow (e) {
    let isDragging = false;
    let xOffset, yOffset;
 
-   newWindow.addEventListener("pointerdown", (e) => {
+   headerTitle.addEventListener("pointerdown", (e) => {
       newWindow.style.zIndex = ++globalZIndex;
       isDragging = true;
 
@@ -35,18 +79,18 @@ function openWindow (e) {
 
       // lock the cursor event to the window
       // prevents loss of control of the window
-      newWindow.setPointerCapture(e.pointerId);
+      headerTitle.setPointerCapture(e.pointerId);
    })
 
-   newWindow.addEventListener("pointerup", (e) => {
+   headerTitle.addEventListener("pointerup", (e) => {
       isDragging = false;
 
       // releases the cursor event since the dragging
       // is now done
-      newWindow.releasePointerCapture(e.pointerId);
+      headerTitle.releasePointerCapture(e.pointerId);
    })
 
-   newWindow.addEventListener("pointermove", (e) => {
+   headerTitle.addEventListener("pointermove", (e) => {
       if(!isDragging) return;
 
       let newX = e.clientX - xOffset;
@@ -78,5 +122,9 @@ function openWindow (e) {
       // of dragging
       currentX = newX;
       currentY = newY;
+   })
+
+   closeScreenButton.addEventListener("click", () => {
+      newWindow.remove();
    })
 }
