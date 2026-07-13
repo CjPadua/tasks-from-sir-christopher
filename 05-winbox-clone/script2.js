0549cb0119f95a8378b9df2941db18b5253fe3b7
Cedie James A. Padua <cediejamesapadua@gmail.com>
2026-07-13 16:31:38 +0800

    Initialize refactoring of html code of winbox

---
+++ 05-winbox-clone/script2.js
@@ -0,0 +1,7 @@
+const openWindowBtn = document.getElementById("open-window-btn");
+const mainContainer = document.getElementById("main");
+const tabContainer = document.getElementById("tab-bar");
+
+let globalZIndex = 10;
+let windowCount = 0;
+
